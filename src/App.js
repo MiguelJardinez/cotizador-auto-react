e881b36df078259e42bb46b19e9827e9c5eb655d
jardinez7514@hotmail.com <jardinez7514@hotmail.com>
2020-04-08 15:11:48 -0500

    Se inicia nuev proyecto en react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './assets/styles/index.scss'
+
+import Prueba from './Components/Prueba'
+
+
+function App() {
+  return (
+    <div>
+      <Prueba />
+    </div>
+  );
+}
+
+export default App;
